1ba719d3f7e1a7e836159998a6d572f9f571011e
penhison <penhison@gmail.com>
2019-05-04 19:23:07 +0800

    添加ycsb对leveldb的测试

--- Programming-FPTree/src/lycsb.cpp
+++ Programming-FPTree/src/lycsb.cpp
@@ -1,18 +1,22 @@
 #include <leveldb/db.h>
 #include <string>
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
 
 #define KEY_LEN 8
 #define VALUE_LEN 8
 using namespace std;
 
-const string workload = "";
+const string workload = "../workloads/";
 
-const string load = workload + ""; // TODO: the workload_load filename
-const string run  = workload + ""; // TODO: the workload_run filename
+const string load = workload + "220w-rw-50-50-load.txt"; // TODO: the workload_load filename
+const string run  = workload + "220w-rw-50-50-run.txt"; // TODO: the workload_run filename
 
-const string filePath = "";
+const string filePath = "/tmp/testdb";
 
-const int READ_WRITE_NUM = 0; // TODO: how many operations
+const int READ_WRITE_NUM = 350000; // TODO: how many operations
 
 int main()
 {        
@@ -20,40 +24,107 @@ int main()
     leveldb::Options options;
     leveldb::WriteOptions write_options;
     // TODO: open and initial the levelDB
+
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, filePath, &db);
+    if (!status.ok()) {
+        cerr << status.ToString() << endl;
+        exit(1);
+    }
+
     uint64_t inserted = 0, queried = 0, t = 0;
     uint64_t* key = new uint64_t[2200000]; // the key and value are same
     bool* ifInsert = new bool[2200000]; // the operation is insertion or not
-	FILE *ycsb_load, *ycsb_run; // the files that store the ycsb operations
-	char *buf = NULL;
-	size_t len = 0;
+    FILE *ycsb_load, *ycsb_run; // the files that store the ycsb operations
+    char *buf = NULL;
+    size_t len = 0;
     struct timespec start, finish; // use to caculate the time
     double single_time; // single operation time
 
     printf("Load phase begins \n");
     // TODO: read the ycsb_load and store
+    ycsb_load = fopen(load.c_str(), "r");
+    if (ycsb_load == NULL) {
+        printf("read load file failed\n");
+        exit(1);
+    } else {
+        printf("read load file success\n");
+    }
+    char op[7];  
+    uint64_t k;
+    while (fscanf(ycsb_load, "%s %lu\n", op, &k) != EOF) {
+        key[t] = k;
+        if (strcmp("INSERT", op) == 0) ifInsert[t] = true;
+        else ifInsert[t] = false;
+        t++;
+    }
+    fclose(ycsb_load);
+
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // TODO: load the workload in LevelDB
+    len = 8;
+    for (int i = 0; i < t; ++ i) {
+        status = db->Put(write_options, leveldb::Slice((char*) &key[i], len), leveldb::Slice((char*) &key[i], len));
+        if (!status.ok()) cout << key[t] << " insert failed" << endl;
+        else inserted++;
+    }
 
     clock_gettime(CLOCK_MONOTONIC, &finish);
-	single_time = (finish.tv_sec - start.tv_sec) * 1000000000.0 + (finish.tv_nsec - start.tv_nsec);
+    single_time = (finish.tv_sec - start.tv_sec) * 1000000000.0 + (finish.tv_nsec - start.tv_nsec);
 
     printf("Load phase finishes: %d items are inserted \n", inserted);
     printf("Load phase used time: %fs\n", single_time / 1000000000.0);
     printf("Load phase single insert time: %fns\n", single_time / inserted);
 
-	int operation_num = 0;
-    inserted = 0;		
+    // leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    // for (it->SeekToFirst(); it->Valid(); it->Next()) {
+    //     printf("key: %lld, value: %lld\n", *((uint64_t*)(it->key().data())),*((uint64_t*)(it->value().data())));
+    // }
+
+    int operation_num = 0;
+    inserted = 0;       
 
     // TODO:read the ycsb_run and store
+    t = 0;
+    ycsb_run = fopen(run.c_str(), "r");
+    if (ycsb_run == NULL) {
+        printf("read run file failed\n");
+        exit(1);
+    } else {
+        printf("read run file success\n");
+    }
+    while (fscanf(ycsb_run, "%s %lu\n", op, &k) != EOF) {
+        key[t] = k;
+        if (strcmp("INSERT", op) == 0) ifInsert[t] = true;
+        else ifInsert[t] = false;
+        t++;
+    }
+    fclose(ycsb_run);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // TODO: operate the levelDB
+    len = 8;
+    string val;
+    for (int i = 0; i < t; ++ i) {
+        operation_num++;
+        if (ifInsert[i]) {
+            status = db->Put(write_options, leveldb::Slice((char*) &key[i], len), leveldb::Slice((char*) &key[i], len));
+            if (!status.ok()) cout << key[t] << " insert failed" << endl;
+            else inserted++;
+        } else {
+            status = db->Get(leveldb::ReadOptions(), leveldb::Slice((char*) &key[i], len), &val);
+            if (!status.ok()) cout << key[t] << " read failed" << endl;
+        }
+    }
 
-	clock_gettime(CLOCK_MONOTONIC, &finish);
-	single_time = (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Run phase finishes: %d/%d items are inserted/searched\n", operation_num - inserted, inserted);
-    printf("Run phase throughput: %f operations per second \n", READ_WRITE_NUM/single_time);	
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    single_time = (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    // printf("Run phase finishes: %d/%d items are inserted/searched\n", operation_num - inserted, inserted);
+    // printf("Run phase throughput: %f operations per second \n", READ_WRITE_NUM/single_time);
+
+    printf("Run phase finishes: %d/%d items are inserted/searched\n", inserted, operation_num - inserted);
+    printf("Run phase throughput: %f operations per second \n", operation_num/single_time);    
     return 0;
 }
---
+++ lycsb测试leveldb.md
@@ -0,0 +1,145 @@
+# lycsb测试leveldb
+
+## 关于ycsb
+ycsb全称Yahoo! Cloud Serving Benchmark，是一个键值数据库性能测试的benchmark，细节请看其[github仓库](https://github.com/brianfrankcooper/YCSB)。  
+YCSB大体上分两个步，第一步是读取load文件，插入一定量的数据对数据库进行初始化。第二步是读取run文件，进行数据库相关操作。load和run文件的一条操作语句如下:
+```
+INSERT 6284781860667377211
+```
+上面INSERT表示插入操作，后面是键值。因为FPTreeDB键值对为8bytes-8bytes，所以**只需取这个值的前8字节即可**。为了简单起见，**键和值取相同即可**。
+即将示例的6284781860667377211视为一个uint64_t类型的一个数据类型，同时将其作为插入数据库的键和值。
+
+我们的测试并没有基于ycsb这个项目，只是依照ycsb的测试思路进行数据库性能的测试。对leveldb的ycsb测试源码详见Programming-FPTree/src/lycsb.cpp
+
+## 关于leveldb
+```
+LevelDB is a fast key-value storage library written at Google that provides an ordered mapping from string keys to string values.
+```
+
+leveldb是一个快速的键值对数据库，提供有序的字符串类型的键与字符串类型的值之间的映射。
+
+细节见其[github仓库](https://github.com/google/leveldb)。
+
+以下是leveldb的简单安装与使用（测试环境Ubuntu18.04）。
+
+### 安装
+首先从github上获取leveldb的源码
+```
+git clone https://github.com/google/leveldb.git
+```
+然后编译，注意编译用到了cmake，如果没有cmake,首先安装cmake
+```
+sudo apt-get isntall cmake
+```
+接下来就是真正的编译环节
+```
+cd leveldb
+mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+```
+
+编译完了之后在build文件夹下会出现libleveldb.a文件，我们测试leveldb时使用静态编译，需要用到此文件。
+
+### 使用
+leveldb提供了简洁的api供我们使用，具体教程见[github上的文档](https://github.com/google/leveldb/blob/master/doc/index.md)
+以下是简单的介绍：
+#### 打开数据库：
+```
+#include <cassert>
+#include "leveldb/db.h"
+
+leveldb::DB* db;
+leveldb::Options options;
+options.create_if_missing = true;
+leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+assert(status.ok());
+```
+其中leveldb::Status是leveldb许多函数的返回值，标记了操作的成功与否，可以使用下面的语句测试操作是否成功：
+```
+leveldb::Status s = ...;
+if (!s.ok()) cerr << s.ToString() << endl;
+```
+
+#### 关闭数据库
+```
+delete db;
+```
+其中db是要关闭的数据库的名字（打开数据库时所起的名字）
+
+#### 读写操作
+leveldb提供Get,Put,Delete三个api来操作数据库，操作示例如下:
+```
+std::string value;
+leveldb::Status s = db->Get(leveldb::ReadOptions(), key1, &value);
+if (s.ok()) s = db->Put(leveldb::WriteOptions(), key2, value);
+if (s.ok()) s = db->Delete(leveldb::WriteOptions(), key1);
+```
+key和value都是leveldb::Slice类型，Slice有4种构造函数，如下：
+Slice有2个成员变量：const char* data_;size_t size_;
+```
+Slice() : data_(""), size_(0) { }
+Slice(const char* d, size_t n) : data_(d), size_(n) { }
+Slice(const std::string& s) : data_(s.data()), size_(s.size()) { }
+Slice(const char* s) : data_(s), size_(strlen(s)) { }
+```
+拷贝构造函数不再列举，
+Slice的常用成员函数有：
+```
+const char* data() const { return data_; }
+size_t size() const { return size_; }
+```
+
+以上介绍只是leveldb的基本操作，删减了很多内容，详见leveldb的GitHub的文档描述。
+
+
+## 使用lycsb测试leveldb
+lycsb代码仿照ycsb测试流程来测试leveldb的性能。
+为了方便测试，我们将leveldb源码及编译后的文件放置于我们的代码的根路径。
+
+### 代码流程
+lycsb代码中关键部分如下：
+首先是前面几个变量定义了workload的load文件和run文件，以及leveldb数据库文件的路径：
+```
+const string workload = "../workloads/";
+
+const string load = workload + "220w-rw-50-50-load.txt"; // TODO: the workload_load filename
+const string run  = workload + "220w-rw-50-50-run.txt"; // TODO: the workload_run filename
+
+const string filePath = "/tmp/testdb";
+```
+其中workload是workloads文件夹的路径，load和run是load和run两个文件的路径，filename是leveldb的数据库文件的路径。
+
+接下来就是打开并读取load文件进入内存，开始计时，按照load文件操作leveldb，完成数据的插入，结束计时，输出load过程的统计结果，
+
+然后是读取run文件进入内存，开始计时，按照run文件操作leveldb，完成数据的插入，结束计时，输出run过程的统计结果，
+
+根据以上两个过程的统计结果可以评测leveldb的性能。
+
+但本次lycsb测试过程中并没有对操作的正确性进行验证。
+
+### 测试过程
+首先需要安装并编译leveldb，具体过程按照上述流程操作即可，为了方便使用，我们将leveldb源码及编译后的文件已放置于Programming-FPTree目录下，不用进行手动安装编译leveldb。
+
+接下来是编译lycsb，编译过程中需要使用leveldb的静态链接，编译命令如下：
+```
+g++ lycsb.cpp -o ./bin/lycsb ../leveldb/build/libleveldb.a -lpthread -I ../leveldb/include/
+```
+
+然后准备开始测试，测试之前先把原来可能残余的数据库文件删除：
+```
+rm -rf /tmp/testdb
+```
+
+然后运行编译好的lycsb文件：
+```
+./bin/lycsb
+```
+
+以上测试都已在makefile中写好，使用时只需要到lycsb.cpp中把load和run文件的路径修改为需要的测试文件，然后在Programming-FPTree/src目录下运行
+```
+make lycsbTest
+```
+不需要进行其他操作。
+
+使用220w-rw-50-50-load.txt和220w-rw-50-50-run.txt测试结果如图：
+![](./picture/lycsb_test_220w_rw_50_50.png)
